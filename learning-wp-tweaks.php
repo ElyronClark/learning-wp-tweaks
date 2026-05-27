9539c1aa1914da23ce654dcec44f6a25a0919a3f
elyron <elyronclark1@gmail.com>
2026-05-27 08:39:23 -0700

    Refactor plugin into a class (OOP)

--- learning-wp-tweaks.php
+++ learning-wp-tweaks.php
@@ -2,62 +2,67 @@
 /**
  * Plugin Name: Learning WP Tweaks
  * Description: My first custom plugin for learning Wordpress development.
- * Version: 1.0.0
+ * Version: 2.0.0
  * Author: Elyron Clark
  */
 
-function lwp_thanks_after_post( $content ) {
-    if ( is_single() ) {
-        $content .= '<p><strong>Thanks for reading!</strong></p>';
+// Safety: don't allow this file to be accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+class LWP_Tweaks {
+    /**
+     * Constructor - runs when the object is created
+     * This is where we wire up all our hooks
+     */
+    public function __construct() {
+        add_filter( 'the_content', array( $this, 'add_thanks_message' ) );
+        add_action( 'init', array( $this, 'register_property_cpt' ) );
     }
-    return $content;
+
+    /** 
+     * Append a thank-you message to single posts.
+     */
+    public function add_thanks_message( $content ) {
+        if ( is_single() ) {
+            $content .= '<p><strong>Thanks for reading!</strong></p>';
+        }
+        return $content;
+    }
+
+    /**
+     * Register the property custom post type
+     */
+    public function register_property_cpt() {
+        $args = array(
+            'labels' => array(
+                'name'          => 'Properties',
+                'singular_name' => 'Property',
+                'add_new_item'  => 'Add New Property',
+                'edit_item'     => 'Edit Property',
+            ),
+            'public'        => true,
+            'has_archive'   => true,
+            'menu_icon'     => 'dashicons-admin-home',
+            'supports'      => array( 'title', 'editor', 'thumbnail'),
+            'show_in_rest'  => true,
+            'rewrite'       => array( 'slug' => 'properties'),
+        );
+        register_post_type( 'lwp_property', $args );
+    }
+
+    /**
+     * Runs on plugin activation: register the CPT, then flush rewrite rules.
+     */
+    public function activate() {
+        $this->register_property_cpt();
+        flush_rewrite_rules();
+    }
 }
-add_filter( 'the_content', 'lwp_thanks_after_post');
 
-function lwp_register_property_cpt() {
-    $args = array(
-        'labels' => array(
-            'name'          => 'Properties',
-            'singular_name' => 'Property',
-            'add_new_item'  => 'Add New Property',
-            'edit_item'     => 'Edit Property',
-        ),
-        'public'        => true,
-        'has_archive'   => true,
-        'menu_icon'     => 'dashicons-admin-home',
-        'supports'      => array( 'title', 'editor', 'thumbnail' ),
-        'show_in_rest'  => true,
-        'rewrite'       => array( 'slug' => 'properties' ),
-    );
-    
-    register_post_type( 'lwp_property', $args );
-}
-add_action( 'init', 'lwp_register_property_cpt' );
+//Create the object - this fires the constructor and wires up all hooks.
+$lwp_tweaks = new LWP_Tweaks();
 
-function lwp_activate_plugin() {
-    //Make sure our post type exists at activation time...
-    lwp_register_property_cpt();
-    // ... then rebuild the rewrite ru les so /properties/ works immediately.
-    flush_rewrite_rules();
-}
-register_activation_hook( __FILE__, 'lwp_activate_plugin' );
-
-// function lwp_show_property_fields( $content ) {
-//     // Only a single Property, and only on the main content (not excerpts, feeds, etc.)
-//     if (is_singular( 'lwp_property' ) && in_the_loop() && is_main_query() ) {
-
-//     $price      = get_field( 'price' );
-//     $bedrooms   = get_field( 'bedrooms' );
-
-//     $details    = '<div class="property-detail">';
-//     $details    .= '<p><strong>Price:</strong> $' . esc_html( number_format( $price ) ) . '</p>';
-//     $details    .= '<p><strong>Bedrooms:</strong> ' . esc_html( $bedrooms ) . '</p>';
-//     $details    .= '</div>';
-
-//     //Put our details ABOVE the body content
-//     $content = $details . $content;
-//     } 
-
-//     return $content;
-// }
-// add_filter( 'the_content', 'lwp_show_property_fields' );
+//Activation hook still lives outside the class, but points at the objects method.
+register_activation_hook( __FILE__, array( $lwp_tweaks, 'activate' ) );
